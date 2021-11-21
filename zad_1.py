e3ab028ee7218d9ad65c80525beb1d70fe6029c4
AdaratUE <adam.ratajczak@edu.uekat.pl>
2021-11-21 17:02:56 +0100

    dodanie 1 zadania

--- zad_1.py
+++ zad_1.py
@@ -1,7 +1,7 @@
-def names_function(list_of_names: list):
-    for x in range(len(list_of_names)):
-        print(list_of_names[x])
+def name_surname(name: str, surname: str) -> str:
+    return f"Cześć  {name} {surname}!"
 
 
-names_list = ["Adam", "Ewa", "Jakub", "Jan", "Karol"]
-names_function(names_list)
+greet = name_surname("Adam", "Ratajczak")
+
+print(greet)
--- zad_4.py
+++
@@ -1,3 +0,0 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for x in range(len(numbers), 2):
-        print(numbers[x])
