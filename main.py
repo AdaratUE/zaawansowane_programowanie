3fd7f5228f3dcc2c887aafea7834db4e32d723e8
AdamRatajczak <adam.ratajczak@edu.uekat.pl>
2021-12-18 15:40:12 +0100

    gray

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 
 
 img1 = cv2.imread('Images/dahl.jpg')
-img2 = cv2.imread('Images/mickiewicz.jp')
+img2 = cv2.imread('Images/mickiewicz.jpg')
 img3 = cv2.imread('Images/paint.png')
 img4 = cv2.imread('Images/shutter.png')
 img5 = cv2.imread('Images/social.jpg')
 
-print(pytesseract.image_to_string(img5))
+img_convert = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+print(pytesseract.image_to_string(img_convert))
 
 
