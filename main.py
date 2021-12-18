a65dcb231884235ec85cda144ff375d181e49e59
AdamRatajczak <adam.ratajczak@edu.uekat.pl>
2021-12-18 13:30:15 +0100

    litera

--- main.py
+++ main.py
@@ -8,14 +8,12 @@ api = Api(app)
 
 movies_list = list()
 
-with open('dane/movies.csv', newline='', encoding="utf8") as csvfile:
+with open('Dane/movies.csv', newline='', encoding="utf8") as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',')
     next(spamreader)
     for i, row in enumerate(spamreader):
         movie = Movies(row[0], row[1], row[2])
         movies_list.append(movie.__dict__)
-
-
 
 
 class TodoSimple(Resource):
